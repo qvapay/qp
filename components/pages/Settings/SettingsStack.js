42eff07c05734a01d0d97aa1f8fda8d43a753fb3
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2023-05-07 18:09:34 -0400

    Cached Avatar ✅

---
+++ components/pages/Settings/SettingsStack.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function SettingsStack() {
+    return (
+        <View>
+            <Text>SettingsStack</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
