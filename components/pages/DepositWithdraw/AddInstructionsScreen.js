a571e965181b8da1b8ea4637542a61e78012a106
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2023-05-10 16:07:46 -0400

    Add Instruction on Deposits TODO

--- components/pages/DepositWithdraw/AddInstructionsScreen.js
+++ components/pages/DepositWithdraw/AddInstructionsScreen.js
@@ -5,9 +5,16 @@ export default function AddInstructionsScreen({ route }) {
 
     const { amount, crypto } = route.params;
 
+    // TODO: Add instructions for each payment method
+    // TODO: Add a button to copy the instructions to the clipboard
+    // TODO: Get the wallet address from the QvaPay API
+    // TODO: Show a QR code for the wallet address
+    // TODO: Add a button to copy the wallet address to the clipboard
+    // TODO: Add a button to open the wallet address in the wallet app
+    // TODO: Check for completed payment and show a success message
+
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>AddInstructionsScreen</Text>
             <Text style={styles.text}>Amount: {amount}</Text>
             <Text style={styles.text}>Crypto: {crypto}</Text>
         </View>
