91a38c59a5f64d5d2972c8e531c72a1fae35eecb
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2023-07-12 19:27:21 -0500

    Shop Item Screen

--- components/pages/Shop/ShopItemScreen.js
+++ components/pages/Shop/ShopItemScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import FastImage from 'react-native-fast-image';
 import { getProductByUuid } from '../../../utils/QvaPayClient';
 import { useNavigation } from '@react-navigation/native';
@@ -27,41 +27,41 @@ export default function ShopItemScreen({ route }) {
     return (
         <View style={globalStyles.container}>
 
-            <View style={[styles.featuredCard, { backgroundColor: color }]}>
-                <FastImage
-                    style={styles.logo}
-                    source={{ uri: `${logo_url}` }}
-                    resizeMode={FastImage.resizeMode.contain}
-                />
-            </View>
+            <ScrollView>
 
-            <View style={styles.productData}>
-
-                <View style={styles.leadContainer}>
-                    <Text style={styles.leadText}>{lead}</Text>
+                <View style={[styles.featuredCard, { backgroundColor: color }]}>
+                    <FastImage
+                        style={styles.logo}
+                        source={{ uri: `${logo_url}` }}
+                        resizeMode={FastImage.resizeMode.contain}
+                    />
                 </View>
 
-                <Text style={globalStyles.title}>{name}</Text>
-                
-                <Text style={globalStyles.subtitle}>{price}</Text>
-                <Text style={globalStyles.subtitle}>{tax}</Text>
-                <Text style={globalStyles.subtitle}>{desc}</Text>
-                <Text style={globalStyles.subtitle}>{meta}</Text>
-                <Text style={globalStyles.subtitle}>{category}</Text>
-            </View>
+                <View style={styles.productData}>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <View style={styles.leadContainer}>
+                            <Text style={styles.leadText}>{lead}</Text>
+                        </View>
+                        <View style={styles.taxContainer}>
+                            <Text style={styles.taxText}>{tax}%</Text>
+                        </View>
+                    </View>
+                    <Text style={globalStyles.title}>{name}</Text>
+                    <Text style={styles.descText}>{`${desc}`}</Text>
+                    <Text style={globalStyles.subtitle}>{category}</Text>
+                </View>
+
+            </ScrollView>
 
             <View style={styles.buyBottom}>
-
-                <View style={{ flexDirection: 'row', alignContent: 'center' }}>
+                <View style={{ flexDirection: 'row', alignContent: 'center', paddingRight: 5 }}>
                     <QPButton title="-1" />
                     <QPButton title="1" />
                     <QPButton title="+1" />
                 </View>
-
                 <View style={{ flex: 1 }}>
                     <QPButton title="Comprar" />
                 </View>
-
             </View>
 
         </View>
@@ -83,7 +83,6 @@ const styles = StyleSheet.create({
     },
     productData: {
         flex: 1,
-        alignItems: 'flex-start',
     },
     buyBottom: {
         flexDirection: 'row',
@@ -97,9 +96,26 @@ const styles = StyleSheet.create({
         paddingHorizontal: 8,
         backgroundColor: theme.darkColors.success
     },
+    taxContainer: {
+        borderRadius: 10,
+        marginVertical: 10,
+        paddingVertical: 4,
+        paddingHorizontal: 8,
+        backgroundColor: theme.darkColors.success
+    },
     leadText: {
         fontSize: 13,
         color: '#fff',
         fontFamily: 'Rubik-Bold',
+    },
+    taxText: {
+        fontSize: 13,
+        color: '#fff',
+        fontFamily: 'Rubik-ExtraBold',
+    },
+    descText: {
+        fontSize: 16,
+        color: '#fff',
+        fontFamily: 'Rubik-Regular',
     },
 })
