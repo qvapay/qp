56a55fae8d76a5516810ea04fc292b8a864ff542
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2023-07-25 22:47:50 -0500

    UserSettings better UI

--- components/pages/Settings/UserdataScreen.js
+++ components/pages/Settings/UserdataScreen.js
@@ -10,9 +10,7 @@ import AvatarPicture from '../../ui/AvatarPicture';
 
 export default function UserdataScreen({ navigation }) {
 
-    // Me object from AppContext
     const { me } = useContext(AppContext);
-
     const [name, setName] = useState(me.name)
     const [lastname, setLastname] = useState(me.lastname)
     const [username, setUsername] = useState(me.username)
@@ -53,80 +51,114 @@ export default function UserdataScreen({ navigation }) {
                     </View>
                 </View>
 
-                <Text style={styles.inputLabelStyle}>Usuario:</Text>
-                <View style={styles.sectionStyle}>
-                    <FontAwesome5 name='lightbulb' size={20} style={{ color: 'white', marginRight: 10 }} />
-                    <TextInput
-                        style={styles.inputStyle}
-                        onChangeText={(username) => setUsername(username)}
-                        underlineColorAndroid="#f000"
-                        value={username}
-                        placeholder="Nombre de Usuario"
-                        placeholderTextColor="#7f8c8d"
-                        autoCapitalize="words"
-                        returnKeyType="next"
-                        blurOnSubmit={false}
-                    />
+                <View style={styles.inputContainer}>
+                    <View style={{ width: 48, alignItems: 'center' }}>
+                        <FontAwesome5 name='user' size={24} style={{ color: 'white', marginRight: 18 }} />
+                    </View>
+                    <View style={{ flex: 1 }}>
+                        <Text style={styles.inputLabelStyle}>Usuario:</Text>
+                        <View style={styles.sectionStyle}>
+                            <TextInput
+                                style={styles.inputStyle}
+                                onChangeText={(username) => setUsername(username)}
+                                underlineColorAndroid="#f000"
+                                value={username}
+                                placeholder="Nombre de Usuario"
+                                placeholderTextColor="#7f8c8d"
+                                autoCapitalize="words"
+                                returnKeyType="next"
+                                blurOnSubmit={false}
+                            />
+                        </View>
+                    </View>
                 </View>
 
-                <Text style={styles.inputLabelStyle}>Nombre:</Text>
-                <View style={styles.sectionStyle}>
-                    <TextInput
-                        style={styles.inputStyle}
-                        onChangeText={(name) => setName(name)}
-                        underlineColorAndroid="#f000"
-                        value={name}
-                        placeholder="Nombre"
-                        placeholderTextColor="#7f8c8d"
-                        autoCapitalize="words"
-                        returnKeyType="next"
-                        blurOnSubmit={false}
-                    />
+                <View style={styles.inputContainer}>
+                    <View style={{ width: 48, alignItems: 'center' }}>
+                        <FontAwesome5 name='user-tag' size={24} style={{ color: 'white', marginRight: 18 }} />
+                    </View>
+                    <View style={{ flex: 1 }}>
+                        <Text style={styles.inputLabelStyle}>Nombre:</Text>
+                        <View style={styles.sectionStyle}>
+                            <TextInput
+                                style={styles.inputStyle}
+                                onChangeText={(name) => setName(name)}
+                                underlineColorAndroid="#f000"
+                                value={name}
+                                placeholder="Nombre"
+                                placeholderTextColor="#7f8c8d"
+                                autoCapitalize="words"
+                                returnKeyType="next"
+                                blurOnSubmit={false}
+                            />
+                        </View>
+                    </View>
                 </View>
 
-                <Text style={styles.inputLabelStyle}>Apellido:</Text>
-                <View style={styles.sectionStyle}>
-                    <TextInput
-                        style={styles.inputStyle}
-                        onChangeText={(lastname) => setLastname(lastname)}
-                        underlineColorAndroid="#f000"
-                        value={lastname}
-                        placeholder="Apellido"
-                        placeholderTextColor="#7f8c8d"
-                        autoCapitalize="words"
-                        returnKeyType="next"
-                        blurOnSubmit={false}
-                    />
+                <View style={styles.inputContainer}>
+                    <View style={{ width: 48, alignItems: 'center' }}>
+                        <FontAwesome5 name='users' size={24} style={{ color: 'white', marginRight: 18 }} />
+                    </View>
+                    <View style={{ flex: 1 }}>
+                        <Text style={styles.inputLabelStyle}>Apellido:</Text>
+                        <View style={styles.sectionStyle}>
+                            <TextInput
+                                style={styles.inputStyle}
+                                onChangeText={(lastname) => setLastname(lastname)}
+                                underlineColorAndroid="#f000"
+                                value={lastname}
+                                placeholder="Apellido"
+                                placeholderTextColor="#7f8c8d"
+                                autoCapitalize="words"
+                                returnKeyType="next"
+                                blurOnSubmit={false}
+                            />
+                        </View>
+                    </View>
                 </View>
 
-                <Text style={styles.inputLabelStyle}>Bio:</Text>
-                <View style={styles.sectionStyle}>
-                    <TextInput
-                        style={styles.inputStyle}
-                        onChangeText={(bio) => setBio(bio)}
-                        underlineColorAndroid="#f000"
-                        value={bio}
-                        multiline={true}
-                        numberOfLines={4}
-                        placeholder="Bio de su perfil"
-                        placeholderTextColor="#7f8c8d"
-                        returnKeyType="next"
-                        blurOnSubmit={false}
-                    />
+                <View style={styles.inputContainer}>
+                    <View style={{ width: 48, alignItems: 'center' }}>
+                        <FontAwesome5 name='signature' size={24} style={{ color: 'white', marginRight: 18 }} />
+                    </View>
+                    <View style={{ flex: 1 }}>
+                        <Text style={styles.inputLabelStyle}>Bio:</Text>
+                        <View style={styles.sectionStyle}>
+                            <TextInput
+                                style={styles.inputStyle}
+                                onChangeText={(bio) => setBio(bio)}
+                                underlineColorAndroid="#f000"
+                                value={bio}
+                                multiline={true}
+                                numberOfLines={4}
+                                placeholder="Bio de su perfil"
+                                placeholderTextColor="#7f8c8d"
+                                returnKeyType="next"
+                                blurOnSubmit={false}
+                            />
+                        </View>
+                    </View>
                 </View>
 
-                <Text style={styles.inputLabelStyle}>Email:</Text>
-                <View style={styles.sectionStyle}>
-                    <TextInput
-                        style={styles.inputStyle}
-                        editable={false}
-                        value={email}
-                        underlineColorAndroid="#f000"
-                        placeholder="Correo Electrónico"
-                        placeholderTextColor="#7f8c8d"
-                        returnKeyType="next"
-                        blurOnSubmit={false}
-                    />
+                <View style={styles.inputContainer}>
+                    <View style={{ width: 48, alignItems: 'center' }}>
+                        <FontAwesome5 name='envelope' size={24} style={{ color: 'white', marginRight: 18 }} />
+                    </View>
+                    <View style={{ flex: 1 }}>
+                        <Text style={styles.inputLabelStyle}>Email:</Text>
+                        <View style={styles.sectionStyle}>
+                            <TextInput
+                                style={styles.inputStyle}
+                                editable={false}
+                                value={email}
+                                underlineColorAndroid="#f000"
+                                placeholder="Correo Electrónico"
+                                placeholderTextColor="#7f8c8d"
+                                returnKeyType="next"
+                                blurOnSubmit={false}
+                            />
+                        </View>
+                    </View>
                 </View>
 
             </ScrollView>
@@ -138,24 +170,30 @@ export default function UserdataScreen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-    sectionStyle: {
-        height: 50,
-        marginTop: 10,
+    inputContainer: {
+        flex: 1,
+        padding: 10,
         flexDirection: 'row',
         alignItems: 'center',
+    },
+    sectionStyle: {
+        flex: 1,
+        height: 40,
+        alignItems: 'center',
+        flexDirection: 'row',
     },
     inputStyle: {
         flex: 1,
         color: 'white',
         borderWidth: 1,
-        paddingHorizontal: 15,
         borderRadius: 10,
+        paddingVertical: 5,
+        paddingHorizontal: 15,
         borderColor: theme.darkColors.elevation,
         fontFamily: "Rubik-Regular",
     },
     inputLabelStyle: {
         fontSize: 14,
-        marginTop: 16,
         color: '#7f8c8d',
         fontFamily: "Rubik-Regular",
     },
@@ -166,7 +204,7 @@ const styles = StyleSheet.create({
         fontFamily: "Rubik-Regular",
     },
     successTextStyle: {
-        fontSize: 18,
+        fontSize: 14,
         color: 'white',
         textAlign: 'center',
         fontFamily: "Rubik-Regular",
