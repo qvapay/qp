84a349741ed8ea9ffdff6bd31f3ebb54923dbb9e
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2023-08-03 13:33:45 -0500

    Improved QPButton

--- components/ui/QPButton.js
+++ components/ui/QPButton.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Pressable, Text, StyleSheet, Vibration } from 'react-native'
-
 import { theme } from './Theme';
 import LinearGradient from 'react-native-linear-gradient';
 
@@ -9,6 +8,7 @@ const GENTLE_VIBRATION_PATTERN = [0, 50];
 export default function QPButton(props) {
 
     const { danger, disabled, title, onPress, style } = props;
+    const gradientColors = danger ? ['#FF4D4D', theme.darkColors.danger] : [theme.darkColors.primary, '#4537D1'];
 
     const handlePress = () => {
         Vibration.vibrate(GENTLE_VIBRATION_PATTERN);
@@ -16,8 +16,13 @@ export default function QPButton(props) {
     };
 
     return (
-        <Pressable onPress={handlePress} style={({ pressed }) => [disabled ? styles.disabledStyle : styles.buttonStyle, { backgroundColor: danger ? theme.darkColors.danger : theme.darkColors.primary }, { transform: [{ scale: pressed ? 0.96 : 1 }] }, { ...style }]}>
-            <LinearGradient colors={['#6759EF', '#4537D1']} style={styles.gradient}>
+        <Pressable
+            onPress={handlePress}
+            style={({ pressed }) => [disabled ? styles.disabledStyle : styles.buttonStyle,
+            { transform: [{ scale: pressed ? 0.96 : 1 }] },
+            { ...style }]}
+        >
+            <LinearGradient colors={gradientColors} style={styles.gradient}>
                 <Text style={styles.titleStyle}>{title}</Text>
             </LinearGradient>
         </Pressable>
